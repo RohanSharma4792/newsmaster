c5df7670e5d56fa42902f94666cbbd18a07f4131
RohanSharma4792 <rohan.sharma2020@vitbhopal.ac.in>
2023-04-08 23:38:31 +0530

    cards left

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from "react";
+import alanBtn from "@alan-ai/alan-sdk-web";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const alanKey = 'dc0f8ce105bca8f36f80ec9d07e5ddc62e956eca572e1d8b807a3e2338fdd0dc/stage'
+const App = () => {
+    useEffect(()=>{
+        alanBtn({
+            key: alanKey,
+            onCommand: ({command, articles}) =>{
+                if(command==='newHeadlines'){
+                    alert('the code executed')
+                }
+            }
+        })
+    }, [])
+
+    return(
+        <div>
+            <h1 style={{color:"black"}}>News By Alan AI</h1>
+        </div>
+    )
 }
-
 export default App;
